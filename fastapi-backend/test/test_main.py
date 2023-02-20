a045c33c4fec62b577985aa8ff22ceae7e56b98f
mariealeth <marie-aleth.andre@centraliens.net>
2023-02-20 11:52:34 +0100

    Ajout tests

---
+++ fastapi-backend/test/test_main.py
@@ -0,0 +1,28 @@
+from main import app
+import unittest
+from fastapi.testclient import TestClient
+import pandas as pd
+import pickle
+import lightgbm
+
+with open("lightGBM_definitif.pkl", "rb") as f:
+    lightGBM_definitif = pickle.load(f)
+    
+
+class MyTestCase(unittest.TestCase):
+    
+    def test_proba1(self):
+        some_customers = pd.read_csv("some_customers.csv")
+        customer_data = some_customers[some_customers['SK_ID_CURR']==100001]
+        customer_data = customer_data.iloc[:, 1: -1].values
+        result = lightGBM_definitif.predict_proba(customer_data)[0][1]
+        self.assertGreater(0.5, result) 
+        
+    def test_proba2(self):
+        some_customers = pd.read_csv("some_customers.csv")
+        customer_data = some_customers[some_customers['SK_ID_CURR']==101780]
+        customer_data = customer_data.iloc[:, 1: -1].values
+        result = lightGBM_definitif.predict_proba(customer_data)[0][1]
+        self.assertGreater(result, 0.6) 
+
+        
